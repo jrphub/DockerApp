317485063f0c368f55c8ae4cf4999d87ee3c0ef5
jrphub <jyotiranjanpattnaik@gmail.com>
2019-03-24 22:25:51 +0530

    batch consumer changes

--- start-batch-consumer.sh
+++ start-batch-consumer.sh
@@ -6,7 +6,7 @@ BATCH_CONSUMER_GROUPID=tweets-batch-group
 OUTPUT_DIR=/tweetsBatchOutput
 
 #Batch Consumer
-docker exec -it $(docker-compose ps -q spark) bash mkdir $OUTPUT_DIR && chmod 777 -R $OUTPUT_DIR
+docker exec -it $(docker-compose ps -q spark) mkdir -p $OUTPUT_DIR && chmod 777 -R $OUTPUT_DIR
 
 
 docker exec -it $(docker-compose ps -q spark) java -jar /apps/LambdaBatchConsumer-0.0.1-SNAPSHOT.jar $KAFKA_HOST $BATCH_CONSUMER_GROUPID $TOPIC $OUTPUT_DIR
