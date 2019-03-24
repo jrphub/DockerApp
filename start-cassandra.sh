875a82956e0a8c54889dd8e27c61ad10cb362ded
jrphub <jyotiranjanpattnaik@gmail.com>
2019-03-24 22:07:53 +0530

    keyspace added

--- start-cassandra.sh
+++ start-cassandra.sh
@@ -2,6 +2,6 @@
 docker exec -it $(docker-compose ps -q cassandra) cqlsh -e "CREATE KEYSPACE gis WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1 };"
 
 
-docker exec -it $(docker-compose ps -q cassandra) cqlsh -e "CREATE TABLE tweets (id text, text text, createdAt text, source text, lang text, sentiment text, fullText text, location text, country text PRIMARY KEY);"
+docker exec -it $(docker-compose ps -q cassandra) cqlsh -e "CREATE TABLE gis.tweets (id text, text text, createdAt text, source text, lang text, sentiment text, fullText text, location text, country text PRIMARY KEY);"
 
 docker exec -it $(docker-compose ps -q cassandra) cqlsh -e "select * from gis.tweets;"
