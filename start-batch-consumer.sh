8242902aece1d589446d1d50ac1890cfe63756e9
jrphub <jyotiranjanpattnaik@gmail.com>
2019-03-24 22:17:02 +0530

    command change for batch consumer

--- start-batch-consumer.sh
+++ start-batch-consumer.sh
@@ -3,8 +3,9 @@ TOPIC=tweets-sentiment
 MASTER=local[*]
 KAFKA_HOST=kafka:9092
 BATCH_CONSUMER_GROUPID=tweets-batch-group
-OUTPUT_DIR=/home/ubuntu/tweetsBatchOutput
+OUTPUT_DIR=/tweetsBatchOutput
 
 #Spark-streaming
 
-docker exec -it $(docker-compose ps -q spark) bash spark-submit --class com.lambda.consumer.LambdaBatchConsumer.App --master $MASTER /apps/LambdaBatchConsumer-0.0.1-SNAPSHOT.jar $KAFKA_HOST $BATCH_CONSUMER_GROUPID $TOPIC $OUTPUT_DIR
+docker exec -it $(docker-compose ps -q spark) bash java -jar /apps/LambdaBatchConsumer-0.0.1-SNAPSHOT.jar $KAFKA_HOST $BATCH_CONSUMER_GROUPID $TOPIC $OUTPUT_DIR
+
